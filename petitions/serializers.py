ee865af688273b09b56605e6e1cc70764b94de79
Roman Rader <roman.rader@gmail.com>
2015-10-08 12:33:06 +0300

    Adjust petition serializer to return author as nested object

--- petitions/serializers.py
+++ petitions/serializers.py
@@ -4,15 +4,20 @@ from petitions.models import Petition
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    petitions = serializers.PrimaryKeyRelatedField(many=True, queryset=Petition.objects.all())
     class Meta:
         model = User
         fields = ('url', 'username')
 
 
+class UserSerializerDetail(serializers.HyperlinkedModelSerializer):
+    petitions = serializers.PrimaryKeyRelatedField(many=True, queryset=Petition.objects.all())
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'petitions')
+
+
 class PetitionSerializer(serializers.HyperlinkedModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
-    responsible = serializers.ReadOnlyField(source='responsible.username')
+    author = UserSerializer(read_only=True)
     class Meta:
         model = Petition
         fields = ('title', 'text', 'author', 'deadline', 'responsible')
--- petitions/views.py
+++ petitions/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from petitions.serializers import UserSerializer, PetitionSerializer
+from petitions.serializers import UserSerializerDetail, PetitionSerializer
 from petitions.models import Petition
 from rest_framework import viewsets, permissions
 from petitions.permissions import IsAuthorOrReadOnly
@@ -7,7 +7,7 @@ from petitions.permissions import IsAuthorOrReadOnly
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
+    serializer_class = UserSerializerDetail
 
 
 class PetitionViewSet(viewsets.ModelViewSet):
