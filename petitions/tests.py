290792ca183357a3b62fe7ed1c431c16d4911693
Vitaliy Franchook <vfranchook@gmail.com>
2015-10-19 20:55:50 +0300

    Added test for tags. In progress

--- petitions/tests.py
+++ petitions/tests.py
@@ -53,6 +53,7 @@ class TestPetitionsResource(unittest.TestCase):
         self.client.logout()
         petition = PETITION.copy()
         petition.update({"media": []})
+        petition.update({"tags": []})
         response = self.client.post(reverse('petition-list'), data=petition, format="json")
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
@@ -64,6 +65,7 @@ class TestPetitionsResource(unittest.TestCase):
         self.client.force_authenticate(self.get_user())
         petition = PETITION.copy()
         petition.update({"media": [{"mediaUrl": "http://example.com/image.jpg", "type": "image"}]})
+        petition.update({"tags": []})
 
         count_before_post = len(Media.objects.all())
         response = self.client.post(reverse('petition-list'), data=petition, format="json")
@@ -78,6 +80,7 @@ class TestPetitionsResource(unittest.TestCase):
         self.client.force_authenticate(self.get_user())
         petition = PETITION.copy()
         petition.update({"media": [{"mediaUrl": "http://example.com/image.jpg", "type": "image"}]})
+        petition.update({"tags": []})
 
         response = self.client.post(reverse('petition-list'), data=petition, format="json")
         response_data = json.loads(response.content.decode())
@@ -99,6 +102,7 @@ class TestPetitionsResource(unittest.TestCase):
         self.client.force_authenticate(self.get_user())
         petition = PETITION.copy()
         petition.update({"media": []})
+        petition.update({"tags": []})
 
         response = self.client.post(reverse('petition-list'), data=petition, format="json")
         response_data = json.loads(response.content.decode())
@@ -118,6 +122,7 @@ class TestPetitionsResource(unittest.TestCase):
         self.client.force_authenticate(self.get_user())
         petition = PETITION.copy()
         petition.update({"media": []})
+        petition.update({"tags": []})
 
         response = self.client.post(reverse('petition-list'), data=petition, format="json")
         response_data = json.loads(response.content.decode())
@@ -131,6 +136,8 @@ class TestPetitionsResource(unittest.TestCase):
 
         petition = PETITION.copy()
         petition.update({"media": [{"id": 42, "mediaUrl": "http://example.com/image.jpg", "type": "image"}]})
+        petition.update({"tags": []})
+
         response = self.client.post(reverse('petition-list'), data=petition, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -138,6 +145,7 @@ class TestPetitionsResource(unittest.TestCase):
         self.client.force_authenticate(self.get_user())
         petition = PETITION.copy()
         petition.update({"media": [{"mediaUrl": "http://example.com/image.jpg", "type": "image"}]})
+        petition.update({"tags": []})
 
         response = self.client.post(reverse('petition-list'), data=petition, format="json")
         response_data = json.loads(response.content.decode())
@@ -153,10 +161,41 @@ class TestPetitionsResource(unittest.TestCase):
         self.assertEqual(len(Media.objects.all()), count_before_post - 1)
         self.assertEqual(len(response_data["media"]), 0)
 
+    def test_create_with_tags(self):
+        self.client.force_authenticate(self.get_user())
+        petition = PETITION.copy()
+        petition.update({"media": []})
+        petition.update({"tags": [{"name": "tag1"}, {"name": "tag2"}]})
+        response = self.client.post(reverse('petition-list'), data=petition, format="json")
+        response_data = json.loads(response.content.decode())
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response_data["tags"][0]["name"], "tag1")
+        self.assertEqual(response_data["tags"][1]["name"], "tag2")
+
+    def test_add_tags(self):
+        self.client.force_authenticate(self.get_user())
+        petition = PETITION.copy()
+        petition.update({"media": []})
+        petition.update({"tags": []})
+        response = self.client.post(reverse('petition-list'), data=petition, format="json")
+        response_data = json.loads(response.content.decode())
+
+        petition.update({
+            "url": response_data["url"],
+            "tags": [{"name": "tag1"}],
+        })
+
+        response = self.client.put(response_data["url"], data=petition, format="json")
+        response_data = json.loads(response.content.decode())
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response_data["tags"][0]["name"], "tag1")
+          
     def test_remove_petition(self):
         self.client.force_authenticate(self.get_user())
         petition = PETITION.copy()
         petition.update({"media": [{"mediaUrl": "http://example.com/image.jpg", "type": "image"}]})
+        petition.update({"tags": []})
 
         response = self.client.post(reverse('petition-list'), data=petition, format="json")
         response_data = json.loads(response.content.decode())
